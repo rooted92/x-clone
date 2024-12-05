13a3a53d33c556aa8aa6dce6596fa2123e5e7c87
Pedro Castaneda <petecast76@gmail.com>
2024-12-05 10:00:22 -0800

    feat: lifted state from NewPost to PostsList

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,30 @@
+import { useState } from 'react';
+
 import Post from './Post';
 import classes from './PostsList.module.css';
+import NewPost from './NewPost';
 
 function PostsList() {
+    const [enteredBody, setEnteredBody] = useState('');
+    const [enteredAuthor, setEnteredAuthor] = useState('');
+
+    function bodyChangeHandler(event) {
+        setEnteredBody(event.target.value);
+    }
+
+    function authorChangeHandler (event) {
+        setEnteredAuthor(event.target.value);
+    }
 
     return (
-    <ul className={classes.posts}>
-        <Post author='Pedro Castaneda' body='React.js is awesome!' />
-        <Post author='Manuel' body='Check out the full course!' />
-    </ul>
+        <>
+            <NewPost onBodyChage={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+            <ul className={classes.posts}>
+                <Post author={enteredAuthor} body={enteredBody} />
+                <Post author='Manuel' body='Check out the full course!' />
+            </ul>
+        </>
+
     );
 }
 

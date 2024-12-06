c777ce619862ab324889f5a42e2641eb1eb93178
Pedro Castaneda <petecast76@gmail.com>
2024-12-05 22:30:48 -0800

    fix: added missing post slug to url in request

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -10,6 +10,16 @@ function PostsList({ isPosting, onStopPosting }) {
     const [posts, setPosts] = useState([]);
 
     function addPostHandler(postData) {
+        fetch(
+            'http://localhost:8080/posts',
+            {
+                method: 'POST',
+                body: JSON.stringify(postData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
         setPosts((existingPosts) => {
             return [postData, ...existingPosts];
         });
@@ -29,7 +39,11 @@ function PostsList({ isPosting, onStopPosting }) {
 
 
             {
-                posts.length === 0 && <p className={`${classes.noPosts} text-center font-semibold text-4xl`}>No posts yet. Add one!</p>
+                posts.length === 0 &&
+                <>
+                    <p className={`${classes.noPosts} text-center font-semibold text-4xl`}>No posts yet.</p>
+                    <p className='text-center mt-4'>Add one!</p>
+                </>
             }
             <ul className={classes.posts}>
                 {
